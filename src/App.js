7f032a1f72089a7ef13c9d0ac74edc9e88d4e5d8
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-01-03 13:16:57 +0530

    some style changes and added subscribe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css"
 import "./default.css"
 
-// import Main from './components/main';
 import {HuddleClientProvider, getHuddleClient } from '@huddle01/huddle01-client';
 import MeVideoElem from './components/MeVideoElem';
 import PeerVideoAudioElem from './components/PeerVideoAudioElem';
@@ -10,9 +9,6 @@ import { useHuddleStore } from "@huddle01/huddle01-client/store";
 import {ethers} from "ethers";
 import Web3Modal from "web3modal";
 import * as PushAPI from "@pushprotocol/restapi";
-// import { NotificationItem, chainNameType } from "@pushprotocol/uiweb";
-// import * as PushAPI from "@pushprotocol/restapi";
-// import { HuddleIframe, huddleIframeApp, HuddleAppEvent  } from "@huddle01/huddle01-iframe";
 import Features from "./components/features";
 import Notification from "./components/Notification";
 
@@ -22,24 +18,19 @@ function App() {
 
   const huddleClient = getHuddleClient("702b03a76c58010686023dac1caeb63696b04b1c069ef14405b4ede34ed1586b");
   const peersKeys = useHuddleStore((state) => Object.keys(state.peers));
-  // const lobbyPeers = useHuddleStore((state) => state.lobbyPeers);
   const isJoined = useHuddleStore((state) => state.roomState.joined);
 
   const [walletConnected, setWalletConnected] = useState(false);
   const [address, setAddress] = useState("");
   const [rendered,setRendered] = useState([]);
-
-  
   const [notifications,setNotifications] = useState([]);
   const [recptAddress,setRecptAddress] = useState("");
 
   const [message,setMessage] = useState("")
-  // const [iframe,setIframe]= useState(false);
   const [incall,setIncall] = useState(false);
-  
+  const[subscribed,setSubscribed] = useState(false);
 const [peerMessage,setPeermessage] = useState("Waiting for user to Join in...")
-
-  
+const channelAddress = "0xD7D98e76FcD14689F05e7fc19BAC465eC0fF4161"
   const web3Modal = new Web3Modal({
         network: "goerli",
         providerOptions : {},
@@ -62,12 +53,23 @@ const [peerMessage,setPeermessage] = useState("Waiting for user to Join in...")
     async function connectWallet(){
         const signer = await getProviderOrSigner(true);
         const add = await signer.getAddress()
-        // console.log(typeof(add))
-        // console.log(toString(add))
-        // console.log(add)
+        const subscriptions = await PushAPI.user.getSubscriptions({
+          user: `eip155:5:${add}`, // user address in CAIP
+          env: 'staging'
+        });
+
         setAddress(add);
         setWalletConnected(true);
-        // console.log(signer);
+        for(let i =0; i<subscriptions.length;i++){
+          if(subscriptions[i].channel == channelAddress){
+            console.log("subscription founded")
+            setSubscribed(true);
+          }
+        }
+        if(subscribed){
+          // {renderMeetContainer}
+        }
+
         
         }
 
@@ -117,19 +119,7 @@ const [peerMessage,setPeermessage] = useState("Waiting for user to Join in...")
   
     }
 
-    // const handleToggleRoomLock = async () => {
-    //   if (!huddleClient) {
-    //     console.error('huddleClient is not initialized');
-  
-    //     return;
-    //   }
-    //   if(isJoined){
-    //   await huddleClient.toggleRoomLock();
-    //   console.log("toogled room lock")}
-    //   else{
-    //   console.log("is joined",isJoined)
-    //   console.log("cant toogle host not joined")}
-    // };
+
 
 
     const renderMeetContainer = () =>{
@@ -178,7 +168,7 @@ const [peerMessage,setPeermessage] = useState("Waiting for user to Join in...")
 
 
   async function getNot(){
-        // window.location.reload(false)
+
         const signer = await getProviderOrSigner(true);
         const notificationsFromApi = await PushAPI.user.getFeeds({
             user: `eip155:5:${await signer.getAddress()}`, // user address in CAIP
@@ -188,34 +178,14 @@ const [peerMessage,setPeermessage] = useState("Waiting for user to Join in...")
         console.log("notification: ",notifications)
       const rendered_not = notifications.map((oneNotification, i) => {
         const { 
-            // cta,
             title,
             message
-            // app,
-            // icon,
-            // image,
-            // url,
-            // blockchain,
-            // notification
+
         } = oneNotification;
     
         return (
           <div>
 
-          {/* <NotificationItem
-              key={`notif-${i}`} // any unique id
-              notificationTitle={title}
-              notificationBody={message}
-              cta={cta}
-              app={app}
-              icon={icon}
-              image={image}
-              url={url}
-              theme="light"
-              chainName={blockchain}
-              // chainName={blockchain as chainNameType} // if using Typescript
-              /> */}
-              {/* <button onClick={()=>joinFromNotificaton()}>join Now</button> */}
               <Notification
               key = {`notif-${i}`}
               title = {title}
@@ -253,11 +223,7 @@ const [peerMessage,setPeermessage] = useState("Waiting for user to Join in...")
               
               // apiResponse?.status === 204, if sent successfully!
               console.log('API repsonse: ', apiResponse);
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-              // for Iframe
-              // setIframe(true);
-              // setTimeout(connectWalleta,5000)
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
               setIncall(true)
               handleJoin();
               console.log(isJoined)
@@ -277,33 +243,37 @@ const [peerMessage,setPeermessage] = useState("Waiting for user to Join in...")
           function handleMsgChange(event){
             setMessage(event.target.value);
           }
-          // const url = window.location.href;
-          // console.log("url:",url)
-          // const urlParam = url.split("=");
-          // console.log(urlParam[1])
-          // if(urlParam[1]===true){
-          //   huddleClient.enableWebcam();
-          // }
 
-          // const iframeConfig = {
-          //   roomUrl: "https://iframe.huddle01.com/123",
-          //   height: "600px",
-          //   width: "80%",
-          //   noBorder: false, // false by default
-          // };
-        // function renderIframe(){
-        //   // connectWalleta()
-        //   setIframe(true);
-          
-        // }
-        // huddleIframeApp.methods.connectWallet(address)
-        // huddleIframeApp.on("me-left", ()=>{setIframe(false)});
         function exit(){
           setIncall(false)
           huddleClient.closeRoomForEverybody()
           huddleClient.close();}
         function exitFromNotification(){
           setPeermessage("User declined the call..");
+        }
+        async function subscribe(){
+          const _signer = await getProviderOrSigner(true);
+          const add = await _signer.getAddress();
+          await PushAPI.channels.subscribe({
+            signer: _signer,
+            channelAddress: `eip155:5:${channelAddress}`, // channel address in CAIP
+            userAddress: `eip155:5:${add}`, // user address in CAIP
+            onSuccess: () => {
+             console.log('opt in success');
+            },
+            onError: () => {
+              console.error('opt in error');
+            },
+            env: 'staging'
+          })
+        }
+        function renderSubscribeContainer(){
+          return(
+            <div className="subContainer">
+              <p>Just a One time setup... Subscribe To Get and Send call Requests</p>
+              <button className="btn-mod" onClick={subscribe}>Subscribe</button>
+            </div>
+          )
         }
 
       
@@ -316,11 +286,11 @@ const [peerMessage,setPeermessage] = useState("Waiting for user to Join in...")
             <p className="head-p">A decentralized wallet to wallet video calling platform made using huddle01 and Push protocol</p>
             {renderButton()}
       </div>
-
-      {!incall && <div className="notification-section">
+      {walletConnected && !subscribed && renderSubscribeContainer()}
+      {walletConnected && subscribed && !incall && <div className="notification-section">
       <div className="get-notif">
             <p>Incoming calls..</p>
-            <button onClick={getNot}>get Notification</button>
+            <button className= "btn-sm-mod custom" onClick={getNot} style = {{margin:"1rem"}}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg></button>
             {rendered[0]}
           </div> 
           <div className="form">
@@ -332,9 +302,7 @@ const [peerMessage,setPeermessage] = useState("Waiting for user to Join in...")
           
     </div>}
       {incall && renderMeetContainer()}
-          
-      {/* {(iframe === true) && <HuddleIframe config={iframeConfig} />} */}
-      {/* <button onClick={renderIframe}>get Iframe</button> */}
+   
     {!incall && <Features/>}
     </div>
 
